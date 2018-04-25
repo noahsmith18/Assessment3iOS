077a0743d0058f204f111f2bc7227fa0f6b501cf
Noah Smith <noahsmith@Interapts-MacBook-Pro-34.local>
2018-04-25 13:30:05 -0400

    first commit

---
+++ AssessmentReview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssessmentReview
+//
+//  Created by Noah Smith on 4/25/18.
+//  Copyright © 2018 Noah Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
